230c9c9852fff04c08207bf05570ea3f20281a4d
Ian Allison <iana@pims.math.ca>
2020-10-01 00:09:32 +0000

    Update shell script
    Signed-off-by: Ian Allison <iana@pims.math.ca>

--- prep-sources.sh
+++ prep-sources.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -e
+
+COLOR_END='\e[0m'
+COLOR_RED='\e[0;31m'
 
 # 
 # This script will attempt to download the sources for nginx's source RPM and
@@ -12,17 +16,48 @@ CENTOS_RELEASE="7"
 RELEASEVER="el${CENTOS_RELEASE}"
 ARCH="x86_64"
 
-rm -rf SRPMS SOURCES
+SRPM="nginx-${NGX_VERSION}.${RELEASEVER}.ngx.src.rpm"
 
-SRPM=nginx-$NGX_VERSION.${RELEASEVER}.ngx.src.rpm
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-mkdir ./SRPMS && cd ./SRPMS && \
+# Exit msg
+msg_exit() {
+    printf "${COLOR_RED}@${COLOR_END}"
+    printf "\n"
+    printf "Exiting...\n"
+    exit 1
+}
+
+# Trap
+cleanup() {
+    rm -rf "${DIR}/SRPMS" "${DIR}/SOURCES"
+    msg_exit "Source preparation failed. Check for missing download locations."
+}
+trap "cleanup" ERR INT TERM
+
+rm -rf "${DIR}/SRPMS" "${DIR}/SOURCES"
+
+# Check rpm2cpio
+[[ -z "$(which rpm2cpio)" ]] && msg_exit "rpm2cpio not installed or not in your path"
+
+# Check cpio
+[[ -z "$(which cpio)" ]] && msg_exit "cpio not installed or not in your path"
+
+# Check cpio
+[[ -z "$(which curl)" ]] && msg_exit "curl not installed or not in your path"
+
+
+mkdir -p "${DIR}/SRPMS" && cd "${DIR}/SRPMS" && \
     curl -LO "http://nginx.org/packages/centos/${CENTOS_RELEASE}/SRPMS/${SRPM}" 
-mkdir ../SOURCES && cd ../SOURCES
-rpm2cpio ../SRPMS/${SRPM} | cpio -i
+
+mkdir -p "${DIR}/SOURCES" && cd "${DIR}/SOURCES"
+
+rpm2cpio "${DIR}/SRPMS/${SRPM}" | cpio -i
 
 curl -Lo nginx-http-shibboleth-${NGX_SHIB_VERSION}.tar.gz \
     https://github.com/nginx-shib/nginx-http-shibboleth/archive/${NGX_SHIB_VERSION}.tar.gz
 
-cd ..
-chown -R mockbuild SRPMS SOURCES
+if [ "${EUID}" -eq 0 ] ; then
+    cd "${DIR}"
+    chown -R mockbuild "${DIR}/SRPMS" "${DIR}/SOURCES"
+fi
